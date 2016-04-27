eb59bbb8d8cc16841e8db517eb5acda87d93e1c6
DonaldMcRonald <raunak1997@utexas.edu>
2016-04-26 21:58:43 -0500

    removed redundant files

--- Tableau/nQueensProblem.py
+++
@@ -1,6 +0,0 @@
-"""
-On a chess board filled with numerous queens, given the coordinate of one
-queen, identify how many queens it can hit.
-
-HINT: BEST SOLUTION CAN BE SOLVED IN O(N) TIME
-"""
--- Tableau/numberPrinter.py
+++
@@ -1,4 +0,0 @@
-"""
-Create a function that takes in a number and print's the english version of
-it, ie 123 => One Hundred Twenty Three
-"""
