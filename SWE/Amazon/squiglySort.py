ff11fe6a0b72194e444e74d347d6eda06c74763c
DonaldMcRonald <raunak1997@utexas.edu>
2016-04-27 14:40:39 -0500

    added a problem from amazon

---
+++ SWE/Amazon/squiglySort.py
@@ -0,0 +1,15 @@
+"""
+Given an unordered list/array as input, order it in the following fashion:
+
+Ordinarily when you sort a list, every sequential value, is either greater or
+less than the previous value. Implement a sort that alternates this ie:
+
+    The 2nd number has to be greater than the 1st
+    The 3rd number has to be less    than the 2nd
+    The 4th number has to be greater than the 3rd
+    and so on...
+
+ex: [3,4,8,2,1,7,5,6,9] -> [1,3,2,5,4,7,6,9,8]
+
+(there are many solutions to this)
+"""
---
+++ SWE/Amazon/squiglySortSoln.py
@@ -0,0 +1,6 @@
+inputList = [1,2,3,4,5,6,7,8,9]
+
+def squiglySort(inputList):
+
+squiglySort(inputList)
+print(inputList)
