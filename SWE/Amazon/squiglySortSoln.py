d9de9a5cb774136f8d7d5feaa6439befb55a6421
DonaldMcRonald <raunak1997@utexas.edu>
2016-04-27 14:53:00 -0500

    added squiqlsort soln

--- SWE/Amazon/squiglySortSoln.py
+++ SWE/Amazon/squiglySortSoln.py
@@ -1,6 +1,18 @@
 inputList = [1,2,3,4,5,6,7,8,9]
 
+def switch(i, j, list):
+    temp = list[i]
+    list[i] = list[j]
+    list[j] = temp
+
 def squiglySort(inputList):
+    for i in range(len(inputList)):
+        if i % 2 is 1:
+            if inputList[i] < inputList[i-1]:
+                switch(i-1, i, inputList)
+        elif i is not 0:
+            if inputList[i] > inputList[i-1]:
+                switch(i-1, i, inputList)
 
 squiglySort(inputList)
 print(inputList)
