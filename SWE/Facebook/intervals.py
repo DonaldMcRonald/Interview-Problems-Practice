33ea0b4e7cf666a6bb2c139a94bf0402c72dcde8
Max Knutsen <mknutsen99@gmail.com>
2016-04-26 23:14:55 -0400

    added facebook

---
+++ SWE/Facebook/intervals.py
@@ -0,0 +1,11 @@
+"""
+
+an interval two numbers that represent the space between those numbers.
+A list of intervals must be non-intersecting and in ascended order. 
+write:
+1) a method that that validates if a given list of intervals satisfies those conditions
+2) a method that merges two lists of intervals so that they become legal lists.
+   if there is overlap between two intervals, merge them into one interval.
+   (3-8,14-22,41-59) and (5-16, 28-33) = (3-22, 28-33, 41-59)
+
+"""
---
+++ SWE/Facebook/treeToArray.py
@@ -0,0 +1,4 @@
+"""
+Write a method that takes an unbalanced, unsorted binary tree and compresses it into an array and be able to describe (in words) how you would turn this array back into the a binary tree with the same structure
+
+"""
